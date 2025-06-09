3cba118fa31c1260ea4233570b90f82b473f68ed
Irshad Shaikh <irshad.shaikh1992@gmail.com>
2025-06-09 18:48:48 +0530

    Api - Added required model Dtos for authentication

---
+++ Mango.Services.AuthAPI/Models/Dto/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Mango.Services.AuthAPI.Models.Dto
+{
+    public class UserDTO
+    {
+        //only taken essentials properties
+        public string ID { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}
