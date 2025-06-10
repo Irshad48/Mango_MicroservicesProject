c3d90129aac8967d3b864057a7cc4c533aa7b057
Irshad Shaikh <irshad.shaikh1992@gmail.com>
2025-06-10 07:50:57 +0530

    Frontend - Added necessory thing to consume Auth Api

---
+++ Mango.Web/Models/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Mango.Web.Models
+{
+    public class UserDto
+    {
+        //only taken essentials properties
+        public string ID { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}
